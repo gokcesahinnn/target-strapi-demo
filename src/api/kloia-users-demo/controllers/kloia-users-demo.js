618968d7c61839df9361014173e3b2951a60e0e7
gokcesahinnn <111432634+gokcesahinnn@users.noreply.github.com>
2023-07-17 16:16:12 +0300

    first commit

---
+++ src/api/kloia-users-demo/controllers/kloia-users-demo.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * kloia-users-demo controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::kloia-users-demo.kloia-users-demo');
---
+++ src/api/kloia-users-demo/routes/kloia-users-demo.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * kloia-users-demo router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::kloia-users-demo.kloia-users-demo');
